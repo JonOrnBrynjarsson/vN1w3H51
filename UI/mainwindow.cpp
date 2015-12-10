b22ab8c565183d0312f5aea4d5c8b8c214c51df1
bardi15 <bardi15@ru.is>
2015-12-10 12:23:31 +0000

    trigger

--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,85 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAdd_New_Computer_Scientist_triggered()
+{
+    qDebug() << "Add_New_Computer_Scientist";
+}
+
+void MainWindow::on_actionAdd_New_Computer_triggered()
+{
+    qDebug() << "Add_New_Computer";
+}
+
+void MainWindow::on_actionAdd_New_Computer_Type_triggered()
+{
+    qDebug() << "Add New Computer Type";
+
+}
+
+void MainWindow::on_actionAdd_Relations_triggered()
+{
+    qDebug() << "Add New Relations";
+}
+
+void MainWindow::on_actionEdit_a_Computer_Scientist_triggered()
+{
+    qDebug() << "Edit_a_Computer_Scientist";
+}
+
+void MainWindow::on_actionEdit_a_Computer_triggered()
+{
+    qDebug() << "Edit_a_Computer";
+}
+
+void MainWindow::on_actionEdit_a_Computer_Type_triggered()
+{
+    qDebug() << "Edit_a_Computer_Type";
+}
+
+void MainWindow::on_actionRemove_a_Computer_Scientist_triggered()
+{
+    qDebug() << "Remove_a_Computer_Scientist";
+}
+
+void MainWindow::on_actionRemove_a_Computer_triggered()
+{
+    qDebug() << "Remove_a_Computer";
+}
+
+void MainWindow::on_actionRemove_a_Computer_Type_triggered()
+{
+    qDebug() << "Remove_a_Computer_Type";
+}
+
+void MainWindow::on_actionRemove_Relations_triggered()
+{
+    qDebug() << "Remove_Relations";
+}
+
+void MainWindow::on_actionDisplay_a_List_of_Computer_Scientists_triggered()
+{
+    qDebug() << "Display_a_List_of_Computer_Scientists";
+}
+
+void MainWindow::on_actionDisplay_a_List_of_Computers_triggered()
+{
+    qDebug() << "Display_a_List_of_Computers";
+}
+
+void MainWindow::on_actionDisplay_a_List_of_Computer_Types_triggered()
+{
+    qDebug() << "Display_a_List_of_Computer_Types";
+}
+
+void MainWindow::on_actionHelp_triggered()
+{
+    qDebug() << "Help";
+}
+
+void MainWindow::on_actionAbout_Computers_and_Scientists_Database_triggered()
+{
+    qDebug() << "About_Computers_and_Scientists_Database";
 }
--- UI/mainwindow.h
+++ UI/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +15,39 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_actionAdd_New_Computer_Scientist_triggered();
+
+    void on_actionAdd_New_Computer_triggered();
+
+    void on_actionAdd_New_Computer_Type_triggered();
+
+    void on_actionAdd_Relations_triggered();
+
+    void on_actionEdit_a_Computer_Scientist_triggered();
+
+    void on_actionEdit_a_Computer_triggered();
+
+    void on_actionEdit_a_Computer_Type_triggered();
+
+    void on_actionRemove_a_Computer_Scientist_triggered();
+
+    void on_actionRemove_a_Computer_triggered();
+
+    void on_actionRemove_a_Computer_Type_triggered();
+
+    void on_actionRemove_Relations_triggered();
+
+    void on_actionDisplay_a_List_of_Computer_Scientists_triggered();
+
+    void on_actionDisplay_a_List_of_Computers_triggered();
+
+    void on_actionDisplay_a_List_of_Computer_Types_triggered();
+
+    void on_actionHelp_triggered();
+
+    void on_actionAbout_Computers_and_Scientists_Database_triggered();
 
 private:
     Ui::MainWindow *ui;
